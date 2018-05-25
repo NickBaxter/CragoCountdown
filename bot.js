94ff478f15d81e50c87c30ad1fff385e2ac7f58b
Nicholas Baxter <nickbaxter4@gmail.com>
2018-05-25 00:18:49 -0400

    Days till return functionality
    Added the math for calculating the days until return from the current date.

--- bot.js
+++ bot.js
@@ -21,13 +21,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {    
-        //current date
-        var d = new Date();
         //Return Date
-        var r = new Date("8/20/2018");
-
-        var dayDiff = +r.getDate() - +d.getDate();
-        var monthDiff = r.getMonth() - d.getMonth();
+        var returnDate = new Date("8/24/2018");
+        var daysLeft = daysToReturn(returnDate);
 
         //I was curious what the parameters returned.
         logger.info('--------------------------------------------');
@@ -36,38 +32,27 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         logger.info('userID: ' + userID);
         logger.info('channelID: ' + channelID);
         logger.info('message: ' + message);
-        logger.info('evt:' + evt);
-
 
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-
-        daysToReturn(r);
-       
         args = args.splice(1);
+
         switch(cmd) {
             // !ping
             case 'ping':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'A Ding Pong!'
-                });
-            break;
-            case 'time':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Time: ' + d.getTime() + 
-                                '\nYear: ' + d.getFullYear() +
-                                '\nMonth: ' + (+d.getMonth()+1) + 
-                                '\nDay: ' + d.getDate()
+                    message: 'Pong!'
                 });
             break;
             case 'return':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Papa Crago returns home in ' + monthDiff + " months and " + dayDiff + " days" 
+                    message: 'Papa Crago returns home in ' + daysLeft + " days" 
                 });
             break;
+            case 'set':
+                bot.listen
             default:
                 bot.sendMessage({
                     to: channelID,
@@ -79,33 +64,49 @@ bot.on('message', function (user, userID, channelID, message, evt) {
      }
 });
 
-function daysToReturn(date) {
-    date = date ;
-    logger.info('date: ' + date);
+function daysToReturn(returnDate) {
+    //Return Date Variables   
+    var returnMonth = returnDate.getMonth() + 1; //this is plus 1 because JS months start at 0. Strange I know.
+    var returnDay = returnDate.getDate();
+    var returnYear = returnDate.getFullYear();
+    
+    //Current Date Variables
+    var currentDate = new Date();
+    var currentMonth = currentDate.getMonth() + 1;
+    var currentDay = currentDate.getDate();
+    var currentYear = currentDate.getFullYear();
 
-    //this is plus 1 because JS months start at 0. Strange I know.
-    month = date.getMonth() + 1;
-    day = date.getDate();
-    year = date.getFullYear();
+    //Now for the fun math!!!!
+    //calculate the days till return
+    daysInCurrent = daysInMonth(returnMonth) - currentDay;   //numbers of days left in current month.
+    var daysInBetweenMonths = 0; //counter for number of days in all the months in between the current and return month.
+    for(var i = currentMonth+1; i < returnMonth; i++) {
+        daysInBetweenMonths = daysInBetweenMonths + daysInMonth(i);
+    }
+    //Adds it all up to find the days left until crago returns!
+    var daysLeft = daysInCurrent + daysInBetweenMonths + returnDay;
+    return daysLeft;
+}
 
-    //numbers of days to be determined in the current and return months.
-    var daysInCurrent = 0;
-    var daysInReturn = 0;
-
-    logger.info('Year: ' + year + '\nMonth: ' + month + '\nDay: ' + day);
-
+function daysInMonth(month) {
+    var numOfDays = 0;
     if([1, 3, 5, 7, 8, 10, 12].includes(month)) {
         logger.info("This month has 31 days!");
-
+        numOfDays = 31;
     }
     else if([4, 6, 9, 11].includes(month)) {
         logger.info("This month has 30 days!");
+        numOfDays = 30;
     }
     else if(month == 2) {
         logger.info("This month has 29 days......or sometimes 28. Have fun with that one");
+        if(year%4 == 0) {//if the year can be evenly be divide by 4 it is a leap year.....usually
+                numOfDays = 28;
+        }
     }
     else {
         logger.info("It would appear you somehow entered the wrong info. Congrats on breaking the program you lil devil ;)");
+        numOfDays = 31;     
     }
-
+    return numOfDays;
 }
